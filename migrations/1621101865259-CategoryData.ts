eb7b40423caae058722b1126d232c65b2587f5b3
Sebastián <sebasrestrepom@gmail.com>
2021-05-15 13:11:20 -0500

    Miración category

---
+++ migrations/1621101865259-CategoryData.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CategoryData1621101865259 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `INSERT INTO category (name) VALUES
+              ('intereses'),
+              ('aptitudes');`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`TRUNCATE TABLE category`);
+  }
+}
