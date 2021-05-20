b890ff059997d084e0edef933fa6b2c901b0f371
Sebastián <sebasrestrepom@gmail.com>
2021-05-20 16:08:00 -0500

    Swagger Instalation

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,27 @@
 require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('GuideApp')
+    .setDescription('Test vocacional')
+    .setVersion('1.0')
+    .addTag('Vocational test')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api/docs', app, document, {
+    explorer: true,
+    swaggerOptions: {
+      filter: true,
+      showRequestDuration: true,
+    },
+  });
+
   await app.listen(3000);
 }
 bootstrap();
